1ee8da1d0779527935aa7ae33efe04397020d498
KingLee <jinlei.kinglee@qq.com>
2022-06-13 20:24:33 +0800

    添加注释

--- src/main/java/rangel/extaction/fire/RangelExtActionRescue.java
+++ src/main/java/rangel/extaction/fire/RangelExtActionRescue.java
@@ -10,6 +10,8 @@ import adf.core.agent.info.WorldInfo;
 import adf.core.agent.module.ModuleManager;
 import adf.core.component.extaction.ExtAction;
 import adf.core.component.module.algorithm.PathPlanning;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import rangel.extaction.RangelExtAction;
 import rescuecore2.config.NoSuchConfigOptionException;
 import rescuecore2.standard.entities.*;
@@ -20,37 +22,41 @@ import java.util.List;
 
 import static rescuecore2.standard.entities.StandardEntityURN.BLOCKADE;
 
+/**
+ * 动作:救援
+ *
+ * @author 软工20-2金磊
+ */
 public class RangelExtActionRescue extends RangelExtAction {
 
     public RangelExtActionRescue(AgentInfo agentInfo, WorldInfo worldInfo, ScenarioInfo scenarioInfo, ModuleManager moduleManager, DevelopData developData) {
         super(agentInfo, worldInfo, scenarioInfo, moduleManager, developData);
         this.target = null;
-        this.thresholdRest = developData
-                .getInteger("adf.impl.extaction.DefaultExtActionFireRescue.rest", 100);
+        this.thresholdRest = developData.getInteger("adf.impl.extaction.DefaultExtActionFireRescue.rest", 100);
 
         switch (scenarioInfo.getMode()) {
-            case PRECOMPUTATION_PHASE:
-            case PRECOMPUTED:
-            case NON_PRECOMPUTE:
-                this.pathPlanning = moduleManager.getModule(
-                        "RangelExtActionRescue.PathPlanning",
-                        "adf.impl.module.algorithm.DijkstraPathPlanning");
-                break;
+            case PRECOMPUTATION_PHASE, PRECOMPUTED, NON_PRECOMPUTE -> this.pathPlanning = moduleManager.getModule(
+                    "RangelExtActionRescue.PathPlanning",
+                    "adf.impl.module.algorithm.DijkstraPathPlanning");
         }
     }
 
     @Override
     public ExtAction calc() {
         this.result = null;
+        //消防队自己
         FireBrigade agent = (FireBrigade) this.agentInfo.me();
 
+        //如果自己需要休息,
         if (this.needRest(agent)) {
             EntityID areaID = this.convertArea(this.target);
             ArrayList<EntityID> targets = new ArrayList<>();
+            //如果自己现在还有目标,则将其存起来
             if (areaID != null) {
                 targets.add(areaID);
             }
         }
+        //如果当前目标不为空,则计算救援
         if (this.target != null) {
             this.result = this.calcRescue(agent, this.pathPlanning, this.target);
         }
@@ -58,44 +64,67 @@ public class RangelExtActionRescue extends RangelExtAction {
     }
 
 
-    private Action calcRescue(FireBrigade agent, PathPlanning pathPlanning,
-                              EntityID targetID) {
+    /**
+     * 计算救援
+     *
+     * @param agent        执行动作的消防队
+     * @param pathPlanning 路径规划
+     * @param targetID     当前的目标
+     * @return adf.core.agent.action.Action 应该执行的动作
+     * @author 软工20-2金磊
+     * @since 2022/6/13
+     */
+    @Nullable
+    private Action calcRescue(FireBrigade agent, PathPlanning pathPlanning, EntityID targetID) {
+        //根据实体ID获得目标实体
         StandardEntity targetEntity = this.worldInfo.getEntity(targetID);
         if (targetEntity == null) {
             return null;
         }
+        //获得消防队的位置
         EntityID agentPosition = agent.getPosition();
-        if (targetEntity instanceof Human) {
-            Human human = (Human) targetEntity;
+        //如果目标实体是个人,
+        if (targetEntity instanceof Human human) {
+            //如果目标的位置没有确定,没法去救,直接返回空
             if (!human.isPositionDefined()) {
                 return null;
             }
+            //如果目标的血量为0,救不了了,直接返回空
             if (human.isHPDefined() && human.getHP() == 0) {
                 return null;
             }
+            //获得目标人类的位置
             EntityID targetPosition = human.getPosition();
+            //如果消防队到达目标人类所在的位置,
             if (agentPosition.getValue() == targetPosition.getValue()) {
+                //并且该人类的被掩埋程度大于0,开始救援,返回救援动作
                 if (human.isBuriednessDefined() && human.getBuriedness() > 0) {
                     return new ActionRescue(human);
                 }
+                //否则没有到达目标人类的所在的位置,
             } else {
-                List<EntityID> path = pathPlanning.getResult(agentPosition,
-                        targetPosition);
+                //计算出从自己到目标的路径
+                List<EntityID> path = pathPlanning.getResult(agentPosition, targetPosition);
+                //如果该路径不为空,并且路径的长度大于0,移动到目标所在的位置,返回动作移动
                 if (path != null && path.size() > 0) {
                     return new ActionMove(path);
                 }
             }
             return null;
         }
+        //如果目标实体是路障,
         if (targetEntity.getStandardURN() == BLOCKADE) {
             Blockade blockade = (Blockade) targetEntity;
+            //并且路障的位置是已知的,将目标实体赋值为该路障的所在位置的实体
             if (blockade.isPositionDefined()) {
                 targetEntity = this.worldInfo.getEntity(blockade.getPosition());
             }
         }
+        //如果目标实体是一个区域,
         if (targetEntity instanceof Area) {
-            List<EntityID> path = pathPlanning.getResult(agentPosition,
-                    targetEntity.getID());
+            //计算出从自己到目标的路径
+            List<EntityID> path = pathPlanning.getResult(agentPosition, targetEntity.getID());
+            //如果该路径不为空,并且路径的长度大于0,移动到目标所在的位置,返回动作移动
             if (path != null && path.size() > 0) {
                 return new ActionMove(path);
             }
@@ -104,7 +133,15 @@ public class RangelExtActionRescue extends RangelExtAction {
     }
 
 
-    private boolean needRest(Human agent) {
+    /**
+     * 判断是否需要休息
+     *
+     * @param agent 执行动作的智能体
+     * @return boolean (true需要休息||false不休息)
+     * @author 软工20-2金磊
+     * @since 2022/6/13
+     */
+    private boolean needRest(@NotNull Human agent) {
         int hp = agent.getHP();
         int damage = agent.getDamage();
         if (hp == 0 || damage == 0) {
@@ -118,28 +155,44 @@ public class RangelExtActionRescue extends RangelExtAction {
                 this.kernelTime = -1;
             }
         }
-        return damage >= this.thresholdRest
-                || (activeTime + this.agentInfo.getTime()) < this.kernelTime;
+        return damage >= this.thresholdRest || (activeTime + this.agentInfo.getTime()) < this.kernelTime;
     }
 
 
+    /**
+     * 将目标转换为一个区域,例如:<br>
+     * •获得人所在区域的实体ID<br>
+     * •获得路障所在区域的实体ID
+     *
+     * @param targetID 目标的实体ID
+     * @return rescuecore2.worldmodel.EntityID
+     * @author 软工20-2金磊
+     * @since 2022/6/13
+     */
+    @Nullable
     private EntityID convertArea(EntityID targetID) {
         StandardEntity entity = this.worldInfo.getEntity(targetID);
         if (entity == null) {
             return null;
         }
-        if (entity instanceof Human) {
-            Human human = (Human) entity;
+        //如果目标实体是一个人,
+        if (entity instanceof Human human) {
+            //并且位置已知,
             if (human.isPositionDefined()) {
+                //获得目标的位置
                 EntityID position = human.getPosition();
+                //如果该位置是一个区域,直接返回区域的实体ID
                 if (this.worldInfo.getEntity(position) instanceof Area) {
                     return position;
                 }
             }
+            //如果目标是一个区域,直接返回该区域的实体ID
         } else if (entity instanceof Area) {
             return targetID;
+            //否则如果是一个路障,
         } else if (entity.getStandardURN() == BLOCKADE) {
             Blockade blockade = (Blockade) entity;
+            //并且该路障的位置已知,返回该路障的位置的实体ID
             if (blockade.isPositionDefined()) {
                 return blockade.getPosition();
             }
