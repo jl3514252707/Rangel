587e33ac8ab98991e6da6367bd0490587dab1daa
KingLee <jinlei.kinglee@qq.com>
2022-06-20 19:58:33 +0800

    添加注释

--- src/main/java/rangel/module/algorithm/AStarPathPlanning.java
+++ src/main/java/rangel/module/algorithm/AStarPathPlanning.java
@@ -14,6 +14,11 @@ import rescuecore2.worldmodel.EntityID;
 
 import java.util.*;
 
+/**
+ * 基于AStar算法的路径规划
+ *
+ * @author 软工20-2金磊
+ */
 public class AStarPathPlanning extends PathPlanning {
 
     private Map<EntityID, Set<EntityID>> graph;
@@ -29,9 +34,7 @@ public class AStarPathPlanning extends PathPlanning {
 
 
     private void init() {
-        Map<EntityID,
-                Set<EntityID>> neighbours = new LazyMap<EntityID, Set<EntityID>>() {
-
+        Map<EntityID, Set<EntityID>> neighbours = new LazyMap<>() {
             @Override
             public Set<EntityID> createValue() {
                 return new HashSet<>();
@@ -90,64 +93,65 @@ public class AStarPathPlanning extends PathPlanning {
 
     @Override
     public PathPlanning calc() {
-        // 1
+        //待遍历的节点
         List<EntityID> open = new LinkedList<>();
+        //已经遍历过的节点
         List<EntityID> close = new LinkedList<>();
         Map<EntityID, Node> nodeMap = new HashMap<>();
 
-        // 3
+        //将起点加入open中
         open.add(this.from);
         nodeMap.put(this.from, new Node(null, this.from));
-        close.clear();
 
         while (true) {
-            // 4
-            if (open.size() < 0) {
+            if (open.size() <= 0) {
                 this.result = null;
                 return this;
             }
-
-            // 5
+            //从open中遍历出优先级最高的节点n
             Node n = null;
             for (EntityID id : open) {
                 Node node = nodeMap.get(id);
-
                 if (n == null) {
                     n = node;
                 } else if (node.estimate() < n.estimate()) {
                     n = node;
                 }
             }
-
-            // 6
+            //如果n为终点,
             if (targets.contains(n.getID())) {
-                // 9
                 List<EntityID> path = new LinkedList<>();
+                //则从终点开始逐步追踪parent节点,一直到达起点
                 while (n != null) {
                     path.add(0, n.getID());
                     n = nodeMap.get(n.getParent());
                 }
-
+                //返回找到的结果路径，算法结束
                 this.result = path;
                 return this;
-            }
-            open.remove(n.getID());
-            close.add(n.getID());
-
-            // 7
-            Collection<EntityID> neighbours = this.graph.get(n.getID());
-            for (EntityID neighbour : neighbours) {
-                Node m = new Node(n, neighbour);
-
-                if (!open.contains(neighbour) && !close.contains(neighbour)) {
-                    open.add(m.getID());
-                    nodeMap.put(neighbour, m);
-                } else if (open.contains(neighbour)
-                        && m.estimate() < nodeMap.get(neighbour).estimate()) {
-                    nodeMap.put(neighbour, m);
-                } else if (!close.contains(neighbour)
-                        && m.estimate() < nodeMap.get(neighbour).estimate()) {
-                    nodeMap.put(neighbour, m);
+                //如果n不是终点,
+            } else {
+                //将节点n从open中删除,
+                open.remove(n.getID());
+                //并加入到close中
+                close.add(n.getID());
+                //获取节点n的所有相邻节点
+                Collection<EntityID> neighbours = this.graph.get(n.getID());
+                //遍历节点n所有的邻近节点
+                for (EntityID neighbour : neighbours) {
+                    //邻近节点
+                    Node m = new Node(n, neighbour);
+                    //如果m即不在open也不在close,则将其添加到open中
+                    if (!open.contains(neighbour) && !close.contains(neighbour)) {
+                        open.add(m.getID());
+                        nodeMap.put(neighbour, m);
+                        //如果m在open中,并且m的estimate比原来要小,则更新其父节点
+                    } else if (open.contains(neighbour) && m.estimate() < nodeMap.get(neighbour).estimate()) {
+                        nodeMap.put(neighbour, m);
+                        //如果m不在close中,并且m的estimate比原来要小,则更新其父节点
+                    } else if (!close.contains(neighbour) && m.estimate() < nodeMap.get(neighbour).estimate()) {
+                        nodeMap.put(neighbour, m);
+                    }
                 }
             }
         }
@@ -171,7 +175,7 @@ public class AStarPathPlanning extends PathPlanning {
                 this.cost = from.getCost() + worldInfo.getDistance(from.getID(), id);
             }
 
-            this.heuristic = worldInfo.getDistance(id, targets.toArray(new EntityID[targets.size()])[0]);
+            this.heuristic = worldInfo.getDistance(id, targets.toArray(new EntityID[0])[0]);
         }
 
 
